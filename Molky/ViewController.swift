4f1db6f848251c838d0c7e173055c4177710f937
valred7 <rougevalentin@icloud.com>
2020-09-06 18:27:36 +0200

    Initial Commit

---
+++ Molky/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Molky
+//
+//  Created by Valentin on 06/09/2020.
+//  Copyright © 2020 Valentin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
