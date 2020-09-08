ceec47330f9142bb182a04cdecdcc0fa60919892
valred7 <rougevalentin@icloud.com>
2020-09-08 23:25:36 +0200

    all

---
+++ Molky/GameManager.swift
@@ -0,0 +1,96 @@
+//
+//  GameManager.swift
+//  Molky
+//
+//  Created by Valentin on 06/09/2020.
+//  Copyright © 2020 Valentin. All rights reserved.
+//
+
+import Foundation
+
+struct player {
+    var _score = 0
+    var numberof0 = 0
+    var _isGameOn = true
+    let name:String
+    var resonOfEndGame:Int? // 0 = 3x0; 1= gagné
+    
+    init(nameIs:String) {
+        name = nameIs
+    }
+}
+
+class GameManager{
+    private static var s_instance:GameManager? = nil
+    public static var shared:GameManager{
+        if s_instance == nil {
+            s_instance = GameManager()
+        }
+        return s_instance!
+    }
+    
+    private init() {
+    }
+    
+    var playerList:[player] = []
+    
+    func createPlayer(givenName:String) {
+        playerList.append(player.init(nameIs: givenName))
+    }
+    
+    func newscore(playerNumber n:Int, newScore:Int) {
+        if newScore != 0{
+            playerList[n]._score = playerList[n]._score + newScore
+            playerList[n].numberof0 = 0
+            haswin(n: n)
+        } else {
+            playerList[n].numberof0 = playerList[n].numberof0 + 1
+            if playerList[n].numberof0 == 3 {
+                returnback(n: n)
+            }
+        }
+    }
+    
+    func getPlayerScore(playerNumber n:Int) -> Int {
+        return playerList[n]._score
+    }
+    
+    func getPlayerGameOn(playerNumber n:Int) -> Bool {
+        return playerList[n]._isGameOn
+    }
+    
+    func getPlayerName(playerNumber n:Int) -> String {
+        return playerList[n].name
+    }
+    
+    func getHowManyPlayer() -> Int {
+        return playerList.count
+    }
+    
+    func getPlayerResonOfEndGame(playerNumber n:Int) -> Int?{
+        return playerList[n].resonOfEndGame
+    }
+    
+    private func haswin (n:Int) {
+        if playerList[n]._score == 50{
+            playerList[n]._isGameOn = false
+            playerList[n].resonOfEndGame = 1
+        } else if playerList[n]._score > 50 {
+            playerList[n]._score = 25
+        }
+    }
+    
+    private func returnback(n:Int) {
+        if playerList[n]._score > 25{
+            playerList[n]._score = 25
+            playerList[n].numberof0 = 0
+        } else if playerList[n]._score > 0 {
+            playerList[n]._score = 0
+            playerList[n].numberof0 = 0
+        } else if playerList[n]._score == 0{
+            playerList[n]._isGameOn = false
+            playerList[n].numberof0 = 0
+            playerList[n].resonOfEndGame = 0
+        }
+    }
+}
--- Molky/ViewController.swift
+++ Molky/ViewController.swift
@@ -8,13 +8,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    let playerManager2 = GameManager.shared
 
+    @IBOutlet weak var Ui_tableview: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        Ui_tableview.dataSource=self
+        ScoreStackView.isHidden = false
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return playerManager2.getHowManyPlayer()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : UITableViewCell = Ui_tableview.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        if let TitleLabel = cell.textLabel{
+            TitleLabel.text = String( playerManager2.getPlayerName(playerNumber: indexPath.row))
+        }
+        if let detailLabel = cell.detailTextLabel{
+            detailLabel.text = String( playerManager2.getPlayerScore(playerNumber: indexPath.row))
+        }
+        return cell 
+    }
+    
+    @IBOutlet weak var ScoreFieldOutlet: UITextField!
+    
+    @IBOutlet weak var NameLabel: UILabel!
+            
+    @IBOutlet weak var ScoreStackView: UIStackView!
+    
+    @IBAction func OkButtonPressed(_ sender: Any) {
+        ScoreFieldOutlet.resignFirstResponder()
+        if let score:Int = Int(ScoreFieldOutlet.text ?? "0") {
+              if score <= 12{
+               playerManager2.newscore(playerNumber: 0, newScore: score)
+               if playerManager2.getPlayerGameOn(playerNumber: 0) == false{
+                   ScoreStackView.isHidden = true
+                   if let reason = playerManager2.getPlayerResonOfEndGame(playerNumber: 0),
+                      reason == 1{
+                       let alert = UIAlertController(title: "Bravo", message: "Vous avez gagné", preferredStyle: .alert)
+                       alert.addAction(UIAlertAction(title: "Cool", style: .default, handler: nil))
+                       self.present(alert, animated: true, completion: nil)
+                   } else if let reason = playerManager2.getPlayerResonOfEndGame(playerNumber: 0),
+                   reason == 0{
+                       let alert = UIAlertController(title: "Vous avez perdu", message: "Vous avez marqué 0 points pendant 3 tous consécutifs. \n Vous ferez mieux la prochaine fois !", preferredStyle: .alert)
+                       alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
+                       self.present(alert, animated: true, completion: nil)
+                   }
+               }
+               Ui_tableview.reloadData()
+           } else {
+               let alert = UIAlertController(title: "Veillez entrer un nombre correct", message: "Au molky un score ne peut être supérieur a 12", preferredStyle: .alert)
+               alert.addAction(UIAlertAction(title: "D'accord", style: .default, handler: nil))
+               self.present(alert, animated: true, completion: nil)
+           }
+        }
+    }
 }
 
