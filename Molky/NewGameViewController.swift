322e52a5030fe8ff3330698e9b005edd3ecc0664
valred7 <rougevalentin@icloud.com>
2020-09-14 12:04:22 +0200

    Ecran nouvelle partie prend en charge plusieurs joueurs

--- Molky/NewGameViewController.swift
+++ Molky/NewGameViewController.swift
@@ -9,17 +9,19 @@
 import Foundation
 import UIKit
 
-class NewGameViewControler: UIViewController {
-    
+class NewGameViewControler: UIViewController, UITableViewDataSource{
+
     let gameManager1 = GameManager.shared
     
+    @IBOutlet weak var NameTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        NameTableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func NewGameButtonTouched(_ sender: Any) {
+    @IBAction func NewPlayerButtonTouched(_ sender: Any) {
         let alert = UIAlertController(title: "Nom du joeur", message: "Entrez le nom du joueur", preferredStyle: .alert)
         alert.addTextField { (textField) in
             textField.placeholder = "Nom du joeur"}
@@ -27,12 +29,21 @@ class NewGameViewControler: UIViewController {
             let textField = alert?.textFields![0]
             let name = textField!.text!
             self.gameManager1.createPlayer(givenName: name)
-            self.performSegue(withIdentifier: "Game", sender: nil)
+            self.NameTableView.reloadData()
         }))
         self.present(alert, animated: true, completion: nil)
-        
-        
-        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return gameManager1.getHowManyPlayer()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = NameTableView.dequeueReusableCell(withIdentifier: "NameCell", for: indexPath)
+        if let TitleLabel = cell.textLabel{
+            TitleLabel.text = gameManager1.getPlayerName(playerNumber: indexPath.row)
+        }
+        return cell
     }
     
     
